8a97d6526fc9e0c1ffc34ba0925a46258cb3eeee
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-06-02 16:09:02 +0200

    Update main.js

--- Exercises/Day7_Exercises/Level2/main.js
+++ Exercises/Day7_Exercises/Level2/main.js
@@ -82,3 +82,166 @@ solveQuadEquation;
 //     console.log(newArr);
 // }
 // reverseArr([3, 4, 5])
+
+// 7.Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+
+
+function capitalizeArray(array) {
+    let capitalizedarray = [];
+    array.forEach(element => {
+        capitalizedarray.push(element.toUpperCase())
+    })
+    return capitalizedarray
+}
+x = ["Kerim", "Gurbaz", "Turkey"]
+console.log(capitalizeArray(x));
+
+// solution2
+
+let capitalizeArr = (arr) => {
+    let newArr = [];
+    arr.forEach(element => {
+        newArr.push(element.toUpperCase())
+    })
+    return newArr
+}
+console.log(capitalizeArr(["bonjour", "tout", "le", "monde"]))
+
+
+// 8.Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+
+function addItem(a) {
+    return Array.from(a).join("")
+}
+let item = "Kerim"
+console.log(addItem(item));
+
+// 9.Declare a function name removeItem. It takes an index parameter and it returns an array after removing an item
+
+function remoItem(index) {
+    array = Array.from(index).pop()
+    return array
+}
+let index = "Kerim"
+console.log(remoItem(index));
+
+// or
+let names = ["kerim", "gurbaz", "js"]
+let removitem = (index) => {
+    names.splice(index);
+    return names
+}
+
+console.log(removitem(1));
+
+
+// 10.Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range.
+
+
+sumOfNumbers = (...nums) => {
+    let sum = 0
+    nums.forEach(element => {
+        sum += element
+    })
+    return sum
+};
+
+console.log(sumOfNumbers(1, 2, 3, 4, 5));
+
+
+// 11.Declare a function name sumOfOdds. It takes a number parameter and it adds all the odd numbers in that - range.
+
+sumOfOdds = (...args) => {
+    let sumOdd = 0
+    args.forEach(element => {
+        if (element % 2) {
+            sumOdd += element
+        }
+    })
+    return sumOdd
+};
+
+console.log(sumOfOdds(1, 2, 3, 4, 5, 6));
+
+
+// 12.Declare a function name sumOfEven. It takes a number parameter and it adds all the even numbers in that - range.
+
+sumOfEven = (...args) => {
+    let sumEven = 0;
+    args.forEach(element => {
+        if (element % 2 === 0) {
+            sumEven += element
+        }
+    })
+    return sumEven
+}
+
+console.log(sumOfEven(1, 2, 3, 4, 5, 6));
+
+
+// 13.Declare a function name evensAndOdds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+
+// evensAndOdds(100);
+// The number of odds are 50.
+// The number of evens are 51.
+
+let evensAndOdds = (number) => {
+    let odd = 0
+    let even = 0
+    for (i = 0; i <= number; i++) {
+        if (i % 2) {
+            odd++
+        } else {
+            even++
+        }
+
+    }
+    return `The number of odds are ${odd}, The number of evens are ${even}`
+}
+console.log(evensAndOdds(100));
+
+
+// 14.Write a function which takes any number of arguments and return the sum of the arguments.
+
+// sum(1, 2, 3) // -> 6
+// sum(1, 2, 3, 4) // -> 10
+
+
+let sum = (...args) => {
+    let total = 0
+    args.forEach(element => {
+        total += element
+    })
+    return total
+}
+
+console.log(sum(1, 2, 3, 4));
+
+
+// 15.Writ a function which generates a randomUserIp.
+
+function randomUserIp() {
+    let userId = []
+    for (let i = 0; i < 3; i++) {
+        let rndm = Math.floor(Math.random() * 256)
+        userId.push(rndm.toString() + ".")
+    }
+    return userId.join("")
+}
+let random = Math.floor(Math.random() * 256)
+
+console.log(`IP : ${randomUserIp() + random}`);
+
+
+// solution2
+
+generateRandomIp = () => {
+    let one = Math.floor(Math.random() * 256);
+    let two = Math.floor(Math.random() * 256);
+    let three = Math.floor(Math.random() * 256);
+    let four = Math.floor(Math.random() * 256);
+    return `IP: ${one}:${two}:${three}:${four}`
+}
+console.log(generateRandomIp());
+
