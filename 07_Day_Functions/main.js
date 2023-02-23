9ec5fb9eefc0903b62893dbc45e3959ec4bf3945
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-23 22:41:53 +0100

    Update main.js

--- 07_Day_Functions/main.js
+++ 07_Day_Functions/main.js
@@ -264,4 +264,19 @@ console.log(sumArray([-1, 0, 1])); // 0
 
 
 
+Faktöriyel hesaplayan bir fonksiyon:
+
+
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); // 120
+console.log(factorial(10)); // 3628800
 
