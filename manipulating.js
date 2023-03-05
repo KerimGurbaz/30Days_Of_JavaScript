57a67f21cc68330e1f27c8b5a0dffeef0bf5072c
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-05 22:50:47 +0100

    Update manipulating.js

--- manipulating.js
+++ manipulating.js
@@ -1,3 +1,10 @@
 
 link.href = 'https://developer.mozilla.org';
 link.textContent = 'Mozilla Developer Network';
+
+
+const sect = document.querySelector('section');
+
+
+const para = document.createElement('p');
+para.textContent = 'We hope you enjoyed the ride.';
