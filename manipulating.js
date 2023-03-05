099f811f6c98dcab0e0d29274d72ff7e0bff57ec
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-05 22:51:18 +0100

    Update manipulating.js

--- manipulating.js
+++ manipulating.js
@@ -8,3 +8,10 @@ const sect = document.querySelector('section');
 
 const para = document.createElement('p');
 para.textContent = 'We hope you enjoyed the ride.';
+
+
+sect.appendChild(para);
+
+
+const linkPara = document.querySelector('p');
+linkPara.appendChild(text);
