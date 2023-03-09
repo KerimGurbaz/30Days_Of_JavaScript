a2bb5b54029e117dc58345e81a84ac250719a95f
KerimGurbaz <kerim.grbz.01@gmail.com>
2023-03-09 14:44:56 +0100

    between given numbers

--- 01_Day_Introduction/helloworld.js
+++ 01_Day_Introduction/helloworld.js
@@ -3,3 +3,14 @@ console.log("hello World!!!")
 // for(let i = 0; i<=10; i++){
 //     console.log(i)
 // }
+
+let total = 0
+let totalFunc  = (first, second) =>{
+    for(let i = first; i<second; i++){
+        total +=i
+    }
+    console.log(total)
+    return total;
+}
+
+totalFunc(2,5) // 9
