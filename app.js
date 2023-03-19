1846e0ef59871bc83eee0aa8afce659345b60900
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-19 23:25:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+** Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present. (Javascript
+)
+Examples :
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+
+
 const myh2 = document.querySelector("#myh2")
 const btn = document.querySelector(".btn");
 const hometown = document.querySelector(".hometown");
